5c7cdbd68ba2f30eefba20534cd906b8f967ada7
mariaroselind <your_email@example.com>
2026-07-26 19:32:30 +0530

    switch to sqlite database

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     SECRET_KEY = 'temporary_test_key'
     
-    # Using the new dedicated user and password
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flix_user:Flix12345@127.0.0.1:3306/flixrec_db'
+    # This tells your app to use a simple local file for the database
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
